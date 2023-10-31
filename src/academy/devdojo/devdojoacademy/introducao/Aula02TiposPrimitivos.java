3f08b9f3f7a156b69b8cfa74951394ec99436b17
Etaniel Caus <nielfilosofia@gmail.com>
2023-10-31 06:11:44 -0300

    Tipos primitos + chars

--- src/academy/devdojo/devdojoacademy/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/devdojoacademy/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,16 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, lang, boolean
         int age = 10;
-        System.out.println(age);
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041';
+
+        System.out.println("A idade é " + age+" anos");
+        System.out.println(caractere);
     }
 }
