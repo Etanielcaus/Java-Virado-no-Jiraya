53cfdb0097e7f7270bd60a03343142566fc10e65
Etaniel Caus <nielfilosofia@gmail.com>
2023-10-31 06:50:00 -0300

    exercicio de String

---
+++ src/academy/devdojo/devdojoacademy/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,16 @@
+package academy.devdojo.devdojoacademy.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Etaniel";
+        String endereco = "Azul";
+        double salario = 1870.00;
+        String dataRecebimento = "05/10/2023";
+
+        String relatorio = "Eu " + nome + ", morando no endereco " + endereco +
+                " confirmo que recebi o salario de " + salario + ", na data "
+                + dataRecebimento;
+
+        System.out.println(relatorio);
+    }
+}
