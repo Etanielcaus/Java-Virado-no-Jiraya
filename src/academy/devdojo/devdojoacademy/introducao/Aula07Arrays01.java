b916e166a183b58e4fd98f67318e8ca1b8149e7a
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-09 07:19:02 -0300

    declarando um array int

---
+++ src/academy/devdojo/devdojoacademy/introducao/Aula07Arrays01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.devdojoacademy.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int[] idades= new int[3];
+        idades[0] = 21;
+        idades[1] = 25;
+        idades[2] = 30;
+
+        System.out.println(idades[1]);
+
+    }
+}
