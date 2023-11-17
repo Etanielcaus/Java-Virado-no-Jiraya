8b4a38616be3492820b0a500c25ebb59a025e7ee
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-17 05:54:55 -0300

    Introducao aos metodos em java

---
+++ src/academy/devdojo/devdojoacademy/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,7 @@
+package academy.devdojo.devdojoacademy.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+    public void somarDois(int num1, int num2){
+        System.out.println(num1 + num2);
+    }
+}
---
+++ src/academy/devdojo/devdojoacademy/javacore/test/CalculadoraTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.devdojoacademy.javacore.test;
+
+import academy.devdojo.devdojoacademy.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somarDois(11, 2);
+    }
+}
