f58f077763c07c63ca52f14b717281a3f1941e68
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-17 06:07:20 -0300

    introducao aos parametros dos metodos

--- src/academy/devdojo/devdojoacademy/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/devdojoacademy/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -1,7 +1,11 @@
 package academy.devdojo.devdojoacademy.javacore.Bintroducaometodos.dominio;
 
 public class Calculadora {
-    public void somarDois(int num1, int num2){
+    public void somarDois(){
+        System.out.println(10 + 10);
+    }
+
+    public void somarDoisComParametros(int num1, int num2){
         System.out.println(num1 + num2);
     }
 }
--- src/academy/devdojo/devdojoacademy/javacore/test/CalculadoraTest02.java
+++ src/academy/devdojo/devdojoacademy/javacore/test/CalculadoraTest02.java
@@ -2,10 +2,9 @@ package academy.devdojo.devdojoacademy.javacore.test;
 
 import academy.devdojo.devdojoacademy.javacore.Bintroducaometodos.dominio.Calculadora;
 
-public class CalculadoraTest01 {
+public class CalculadoraTest02 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-        calculadora.somarDois();
-
+        calculadora.somarDoisComParametros(10, 20);
     }
 }
