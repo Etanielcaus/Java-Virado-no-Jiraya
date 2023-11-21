9b7e53d22b2b94ed7cdd872a5a13c60c96aa8417
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-20 22:46:10 -0300

    referencia this

---
+++ src/academy/devdojo/devdojoacademy/javacore/test/EstudanteTest02.java
@@ -0,0 +1,14 @@
+package academy.devdojo.devdojoacademy.javacore.test;
+
+import academy.devdojo.devdojoacademy.javacore.Bintroducaometodos.dominio.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        estudante01.nome = "Goku";
+        estudante01.idade = 18;
+        estudante01.sexo = 'M';
+        estudante01.imprime();
+
+    }
+}
