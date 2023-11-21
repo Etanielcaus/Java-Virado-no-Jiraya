59d0c2d3e107d25a3285578d4f78adb70da3a7b6
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-20 21:59:09 -0300

    tipos reference em methodos pt2

--- src/academy/devdojo/devdojoacademy/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
+++ src/academy/devdojo/devdojoacademy/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -7,5 +7,14 @@ public class ImpressoraEstudante {
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
         System.out.println(estudante.sexo);
+
+        String nomeando = estudante.nome;
+
+
+        if (nomeando.length() > 5){
+            System.out.println(nomeando.charAt(4));
+        } else {
+            System.out.println(nomeando);
+        }
     }
 }
--- src/academy/devdojo/devdojoacademy/javacore/test/EstudanteTest01.java
+++ src/academy/devdojo/devdojoacademy/javacore/test/EstudanteTest01.java
@@ -4,6 +4,12 @@ import academy.devdojo.devdojoacademy.javacore.Bintroducaometodos.dominio.Estuda
 import academy.devdojo.devdojoacademy.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
 
 public class EstudanteTest01 {
+
+    public int modificaValor(int x){
+        x = 10;
+        System.out.println("Valor modificado: " + x);
+        return x;
+    }
     public static void main(String[] args) {
         Estudante estudante01 = new Estudante();
         Estudante estudante02 = new Estudante();
@@ -13,11 +19,23 @@ public class EstudanteTest01 {
         estudante01.idade = 15;
         estudante01.sexo = 'M';
 
-        estudante02.nome = "Alicia";
+        estudante02.nome = "ALICIA";
         estudante02.idade = 19;
         estudante02.sexo = 'F';
 
+        // Aqui ele utiliza o valor do tipo referencia, ou seja, o valor original muda
         imprimirEstudante.imprimirEstudante(estudante01);
         imprimirEstudante.imprimirEstudante(estudante02);
+
+        // Este é do tipo primitivo, ou seja, o valor original não muda
+        int valor = 20;
+        EstudanteTest01 obj = new EstudanteTest01();
+        int novoValor = obj.modificaValor(valor);
+        System.out.println(novoValor);
+        System.out.println(valor);
+
+
     }
+
+
 }
