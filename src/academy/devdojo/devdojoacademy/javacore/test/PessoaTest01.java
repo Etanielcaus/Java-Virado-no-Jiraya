103a814befabbe6890982922c38ea06f9459cfb8
Etaniel Caus <nielfilosofia@gmail.com>
2023-11-22 06:40:47 -0300

    methodos get e set pt01

---
+++ src/academy/devdojo/devdojoacademy/javacore/test/PessoaTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo.devdojoacademy.javacore.test;
+
+import academy.devdojo.devdojoacademy.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+//        pessoa1.idade = 18;
+//        pessoa1.nome = "Goku";
+
+        pessoa1.setIdade(18);
+        pessoa1.setNome("Goku");
+
+        System.out.println(pessoa1.getNome());
+        System.out.println(pessoa1.getIdade());
+
+        pessoa1.imprime();
+    }
+}
