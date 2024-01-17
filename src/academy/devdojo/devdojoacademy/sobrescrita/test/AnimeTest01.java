e4c9b61dd3678ed9504e85a88df1a6f5e14833d8
Etaniel Caus <nielfilosofia@gmail.com>
2024-01-16 22:58:24 -0300

    Sobrescrita

---
+++ src/academy/devdojo/devdojoacademy/sobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.devdojoacademy.sobrescrita.test;
+
+import academy.devdojo.devdojoacademy.sobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Naruto");
+        System.out.println(anime);
+    }
+}
