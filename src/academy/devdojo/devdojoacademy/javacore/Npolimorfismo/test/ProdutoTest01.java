96b6b129d5207976e34f7cd7897f9fb270f1aee0
Etaniel Caus <nielfilosofia@gmail.com>
2024-03-13 22:06:40 -0300

    Polimorfismo - pt03

--- src/academy/devdojo/devdojoacademy/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/academy/devdojo/devdojoacademy/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.devdojoacademy.javacore.Npolimorfismo.test;
 
 import academy.devdojo.devdojoacademy.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.devdojoacademy.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.devdojoacademy.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.devdojoacademy.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,7 +9,10 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador = new Computador("MAC X", 14000);
         Tomate tomate = new Tomate("Tomate Cereja", 10);
-        CalculadoraImposto.calcularImpostoComputador(computador);
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        Televisao tv = new Televisao("Samsung 50\" ", 5000);
+        CalculadoraImposto.calcularImposto(computador);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("=======");
+        CalculadoraImposto.calcularImposto(tv);
     }
 }
