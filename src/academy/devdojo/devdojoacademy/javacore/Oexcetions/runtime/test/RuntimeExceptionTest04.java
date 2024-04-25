cf56767c601fe4da4b4fba0fcd8df776e0413d70
Etaniel Caus <nielfilosofia@gmail.com>
2024-04-24 23:23:23 -0300

    101 - Exceções pt 07 - Capturando múltiplas exceções

--- src/academy/devdojo/devdojoacademy/javacore/Oexcetions/runtime/test/RuntimeExceptionTest04.java
+++ src/academy/devdojo/devdojoacademy/javacore/Oexcetions/runtime/test/RuntimeExceptionTest04.java
@@ -1,14 +1,29 @@
 package academy.devdojo.devdojoacademy.javacore.Oexcetions.runtime.test;
 
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         // Para tratar multiplas exceções, basta utilizar multiplos catchs
         try {
-            throw new ArrayIndexOutOfBoundsException();
+            throw new RuntimeException();
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Dentro do ArrayIndexOutOfBoundsException");
         } catch (IndexOutOfBoundsException e){
             System.out.println("Dentro do IndexOutOfBoundsException");
+        } catch (RuntimeException e){
+            System.out.println("Dentro do RuntimeException");
         }
+
+        try {
+            talvezLanceException();
+        } catch (FileNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void talvezLanceException() throws FileNotFoundException, SQLException {
+
     }
 }
