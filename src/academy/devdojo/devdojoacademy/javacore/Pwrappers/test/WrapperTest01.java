206c2de0fe8586a232603a5fcb5ae7efe1a344c9
Etaniel Caus <nielfilosofia@gmail.com>
2024-04-26 23:30:39 -0300

    106 - Classes Utilitárias - Wrappers pt 01

---
+++ src/academy/devdojo/devdojoacademy/javacore/Pwrappers/test/WrapperTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.devdojoacademy.javacore.Pwrappers.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+//                Integer: Envolve um valor do tipo primitivo int.
+//                Double: Envolve um valor do tipo primitivo double.
+//                Boolean: Envolve um valor do tipo primitivo boolean.
+//                Character: Envolve um valor do tipo primitivo char.
+//                Byte: Envolve um valor do tipo primitivo byte.
+//                Short: Envolve um valor do tipo primitivo short.
+//                Long: Envolve um valor do tipo primitivo long.
+//                Float: Envolve um valor do tipo primitivo float.
+
+        Integer intW = 1;
+        Double doubleW = 1.0;
+        Boolean booleanW = false;
+        Character charW = 'S';
+        Byte byteW = 1;
+        Short shortW = 1;
+        Long longW = 1L;
+        Float floatW = 1F;
+    }
+}
