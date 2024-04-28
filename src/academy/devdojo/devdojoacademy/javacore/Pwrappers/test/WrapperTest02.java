ce0fa2681b4e731d5ee38536ead78eacd6c043b4
Etaniel Caus <nielfilosofia@gmail.com>
2024-04-27 23:55:15 -0300

    107 - Classes Utilitárias - Wrappers pt 02

--- src/academy/devdojo/devdojoacademy/javacore/Pwrappers/test/WrapperTest02.java
+++ src/academy/devdojo/devdojoacademy/javacore/Pwrappers/test/WrapperTest02.java
@@ -4,6 +4,10 @@ public class WrapperTest02 {
     public static void main(String[] args) {
         Integer intW = 12; // boxing
         int i = intW; // unboxing
+
+        Integer intW2 = i;
+        System.out.println(intW2);
+        System.out.println("-===");
         System.out.println(intW.intValue());
         Integer int2W = Integer.parseInt("2");
         System.out.println(int2W);
