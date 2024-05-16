5957310c5381a3003a12fd140d51d7757e584572
Etanielcaus <nielfilosofia@gmail.com>
2024-05-15 22:23:20 -0300

    121 - Classes Utilitárias - LocalDateTime

---
+++ src/academy/devdojo/devdojoacademy/javacore/Rdatas/test/LocalDateTime01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.devdojoacademy.javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Locale;
+
+public class LocalDateTime01 {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.of(2002, 12, 5, 5, 5, 5);
+        System.out.println(localDateTime);
+
+        ZoneId zoneIdBR = ZoneId.of("America/Sao_Paulo");
+        ZoneId zoneIdJP = ZoneId.of("Asia/Tokyo");
+        LocalDateTime localDateTime1 = LocalDateTime.now(zoneIdJP);
+        System.out.println(localDateTime1);
+
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now();
+        LocalDateTime localDateTime2 = LocalDateTime.of(localDate, localTime);
+        System.out.println(localDateTime2);
+
+    }
+}
