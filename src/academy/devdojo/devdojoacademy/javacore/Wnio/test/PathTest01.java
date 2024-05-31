89727a886b8a441edbc4360b39b7f30dd6ec9621
Etanielcaus <nielfilosofia@gmail.com>
2024-05-30 21:46:55 -0300

    144 (Renomeando Pasta)

--- src/academy/devdojo/devdojoacademy/javacore/Wnio/test/PathTest01.java
+++ src/academy/devdojo/devdojoacademy/javacore/Wnio/test/PathTest01.java
@@ -1,12 +1,9 @@
 package academy.devdojo.devdojoacademy.javacore.Wnio.test;
 
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PathTest02 {
+public class PathTest01 {
     public static void main(String[] args) {
         Path path = Paths.get("C:\\Users\\Etaniel\\Desktop\\Desenvolvimento\\Java-Virado-no-Jiraya\\arquivo\\file.txt");
         System.out.println(path.getFileName());
