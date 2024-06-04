ce25bbf898bb9007aba7ca772020107b0cb47137
Etanielcaus <nielfilosofia@gmail.com>
2024-06-03 21:47:13 -0300

    Renomeando arquivo

--- src/academy/devdojo/devdojoacademy/javacore/Wnio/test/ResolveTest03.java
+++ src/academy/devdojo/devdojoacademy/javacore/Wnio/test/ResolveTest03.java
@@ -3,7 +3,7 @@ package academy.devdojo.devdojoacademy.javacore.Wnio.test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PathTest03 {
+public class ResolveTest03 {
     public static void main(String[] args) {
         Path dir = Paths.get("arquivo/home/Etaniel");
         Path arquivo = Paths.get("dev/arquivo.txt");
