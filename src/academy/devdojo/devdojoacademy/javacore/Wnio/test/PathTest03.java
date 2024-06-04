2d1245134c0cd4282ba13cea5057a7a653cd615a
Etanielcaus <nielfilosofia@gmail.com>
2024-06-03 21:32:30 -0300

    146 - Classes Utilitárias - NIO pt 03 - Normalização

---
+++ src/academy/devdojo/devdojoacademy/javacore/Wnio/test/PathTest03.java
@@ -0,0 +1,17 @@
+package academy.devdojo.devdojoacademy.javacore.Wnio.test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathTest03 {
+    public static void main(String[] args) {
+        Path dir = Paths.get("arquivo/home/Etaniel");
+        Path arquivo = Paths.get("dev/arquivo.txt");
+        Path resolve = dir.resolve(arquivo);
+        System.out.println(resolve);
+
+        Path absoluto = Paths.get("/arquivo/home");
+        Path relativo = Paths.get("dev");
+        System.out.println(absoluto.resolve(relativo));
+    }
+}
