b540a19616ff7888e69db57f9178ebc477eacf5d
Etanielcaus <nielfilosofia@gmail.com>
2024-06-20 22:20:17 -0300

    162 - Coleções pt 02 - equals pt 02

--- src/academy/devdojo/devdojoacademy/javacore/YColections/dominio/SmartPhone.java
+++ src/academy/devdojo/devdojoacademy/javacore/YColections/dominio/SmartPhone.java
@@ -9,6 +9,20 @@ public class SmartPhone {
         this.marca = marca;
     }
 
+//    Reflexio> x.equal(x) true para tudo que for diferente de null
+//    sinético: para xe y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
+//    Transitividade: para x,y,z diferentes de null, se x.equals(y) == true logo, y.equals(x) == true, logo, y.equals(z) == true
+//    Consistente: x.equals(x) sempre retorna true se x for deiferente de null
+//    para x diferente de true retorna false
+    @Override
+    public boolean equals(Object obj) {
+         if (obj == null) return false;
+         if (this == obj) return true;
+         if (this.getClass() != obj.getClass()) return false;
+         SmartPhone smartPhone = (SmartPhone) obj;
+         return serialNumber != null && serialNumber.equals(smartPhone.serialNumber);
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
