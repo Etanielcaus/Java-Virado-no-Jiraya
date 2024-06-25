73acc528633a0ca464e753a9613e54a870e96b81
Etanielcaus <nielfilosofia@gmail.com>
2024-06-24 21:43:18 -0300

    165 - Coleções pt 05 - Complexidade Big-O

--- src/academy/devdojo/devdojoacademy/javacore/YColections/test/EqualsTest01.java
+++ src/academy/devdojo/devdojoacademy/javacore/YColections/test/EqualsTest01.java
@@ -11,12 +11,14 @@ public class EqualsTest01 {
 //        System.out.println(nome2.equals(nome));
 
         SmartPhone xiaomi = new SmartPhone("123", "xiaomi");
-        SmartPhone xiaomi2 = new SmartPhone("123", "xiaomi");
+        SmartPhone xiaomi2 = new SmartPhone("123", "xiaom5ti");
         SmartPhone xiaomi3 = xiaomi;
 
         System.out.println(xiaomi2.getMarca().equals(xiaomi.getMarca()));
         System.out.println(xiaomi.equals(xiaomi3));
         System.out.println(xiaomi.hashCode());
+        System.out.println(xiaomi2.hashCode());
+        System.out.println(xiaomi3.hashCode());
 
 
     }
