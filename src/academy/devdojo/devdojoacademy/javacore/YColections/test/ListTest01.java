43965028463c082f50747db1fed27f131a1f6af7
Etanielcaus <nielfilosofia@gmail.com>
2024-06-25 21:14:19 -0300

    167 - Coleções pt 07 - List pt 02

--- src/academy/devdojo/devdojoacademy/javacore/YColections/test/ListTest01.java
+++ src/academy/devdojo/devdojoacademy/javacore/YColections/test/ListTest01.java
@@ -8,21 +8,37 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(16);
+        List<String> nomes2 = new ArrayList<>(16);
         nomes.add("Joao");
         nomes.add("Pedro");
         nomes.add("Maria");
+        nomes.remove(1);
+        nomes2.add("joao");
+        nomes2.add("maria");
 //        nomes.add(123); // não é possivel passar int, pois a List<String> está forçando ser uma String
+
+        nomes.addAll(nomes2);
 
         for (String nome : nomes) {
             System.out.println(nome.toUpperCase());
         }
-        
-        nomes.add("Suane");
+
         System.out.println("=====");
 
-        for (int i = 0; i < nomes.size(); i++) {
+        int size = nomes.size();
+        for (int i = 0; i < size; i++) {
+//            nomes.add("Suane");
             System.out.println(nomes.get(i));
         }
+//        System.out.println(nomes);
+
+        System.out.println("===");
+
+        List<Integer> numeros = new ArrayList<>(16);
+        numeros.add(1);
+        numeros.add(2);
+        System.out.println(numeros);
+
 
     }
 }
