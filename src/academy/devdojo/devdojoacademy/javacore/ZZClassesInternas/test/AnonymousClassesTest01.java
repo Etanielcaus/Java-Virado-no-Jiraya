1295bd1b261265a5d265b75c410ba6b7f9580af0
Etanielcaus <nielfilosofia@gmail.com>
2024-07-12 23:02:03 -0300

    191 - Classes Internas pt 03 - Classes Anônimas

---
+++ src/academy/devdojo/devdojoacademy/javacore/ZZClassesInternas/test/AnonymousClassesTest01.java
@@ -0,0 +1,24 @@
+package academy.devdojo.devdojoacademy.javacore.ZZClassesInternas.test;
+
+class Animal{
+    public void walk(){
+        System.out.println("Animal Walking...");
+    }
+}
+
+//class Dog extends Animal{
+//    public void walk(){
+//        System.out.println("Dog Walking...");
+//    }
+//}
+
+public class AnonymousClassesTest01 {
+    public static void main(String[] args) {
+        Animal a = new Animal(){
+            public void walk(){
+                System.out.println("Walking in the street.");
+            }
+        };
+        a.walk();
+    }
+}
