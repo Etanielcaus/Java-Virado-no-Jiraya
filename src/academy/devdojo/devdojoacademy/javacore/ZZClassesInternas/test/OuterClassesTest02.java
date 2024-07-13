ece29f3a17e7c801011892901ff53f865b7bb198
Etanielcaus <nielfilosofia@gmail.com>
2024-07-12 22:46:02 -0300

    190 - Classes Internas pt 02 - Classes Locais

---
+++ src/academy/devdojo/devdojoacademy/javacore/ZZClassesInternas/test/OuterClassesTest02.java
@@ -0,0 +1,22 @@
+package academy.devdojo.devdojoacademy.javacore.ZZClassesInternas.test;
+
+public class OuterClassesTest02 {
+    private String name = "Pedro";
+
+    void print(String parametro){
+        final String lastName = "Silva";
+        class LocalClass{
+            public void printLocal(){
+                System.out.println(name + " " + lastName);
+                System.out.println(parametro);
+            }
+        }
+        LocalClass local = new LocalClass();
+        local.printLocal();
+    }
+
+    public static void main(String[] args) {
+        OuterClassesTest02 test = new OuterClassesTest02();
+        test.print("Nomes");
+    }
+}
