24ead2ead595f7e32e1d24c6f57ad185962298f5
Etanielcaus <nielfilosofia@gmail.com>
2024-07-14 22:09:59 -0300

    195 - Lambdas pt 01 - Predicate

--- src/academy/devdojo/devdojoacademy/javacore/ZZBcomportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/devdojoacademy/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -2,6 +2,9 @@ package academy.devdojo.devdojoacademy.javacore.ZZBcomportamento.interfaces;
 
 import academy.devdojo.devdojoacademy.javacore.ZZBcomportamento.dominio.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
     boolean test(Car car);
+//    (parametro) -> <expressao>
+//    (Car car) -> function
 }
