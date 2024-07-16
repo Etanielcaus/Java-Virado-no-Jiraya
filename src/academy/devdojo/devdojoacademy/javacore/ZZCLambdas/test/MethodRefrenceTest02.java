f5099bbbd1922c3d02cbc080bc186ea4f7b128cf
Etanielcaus <nielfilosofia@gmail.com>
2024-07-15 21:49:49 -0300

    199 - Method Reference pt 02 - Referência a métodos não estáticos

---
+++ src/academy/devdojo/devdojoacademy/javacore/ZZCLambdas/test/MethodRefrenceTest02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.devdojoacademy.javacore.ZZCLambdas.test;
+
+import academy.devdojo.devdojoacademy.javacore.ZZCLambdas.dominio.Anime;
+import academy.devdojo.devdojoacademy.javacore.ZZCLambdas.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodRefrenceTest02 {
+    public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Bersek", 5), new Anime("Naruto", 10), new Anime("DBZ", 3)));
+
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+//        animeList.sort((a1, a2) -> animeComparators.compareByEpisodesNonStatic(a1,a2));
+        System.out.println(animeList);
+    }
+}
