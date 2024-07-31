c702e06a78a10e46cef8d99f3a0ee9ef95e49ca7
Etanielcaus <nielfilosofia@gmail.com>
2024-07-30 22:50:00 -0300

    242 - Concorrência pt 14 - CompletableFuture pt 03 - ThreadFactory

---
+++ src/academy/devdojo/devdojoacademy/javacore/ZZGconcorrencia/Serveice/StoreServiceDeprecated.java
@@ -0,0 +1,26 @@
+package academy.devdojo.devdojoacademy.javacore.ZZGconcorrencia.Serveice;
+
+import java.util.concurrent.*;
+
+public class StoreServiceDeprecated {
+
+    public double getPriceSync(String storeName) {
+        System.out.println("Getting prices sync for store: " + storeName);
+        return priceGenerator();
+    }
+
+
+    private double priceGenerator() {
+        System.out.printf("%s : generating price %n", Thread.currentThread().getName());
+        delay();
+        return ThreadLocalRandom.current().nextInt(1, 500) * 10;
+    }
+
+    private void delay() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/academy/devdojo/devdojoacademy/javacore/ZZGconcorrencia/test/CompletableFutureTest03.java
@@ -0,0 +1,41 @@
+package academy.devdojo.devdojoacademy.javacore.ZZGconcorrencia.test;
+
+import academy.devdojo.devdojoacademy.javacore.ZZGconcorrencia.Serveice.StoreService;
+import academy.devdojo.devdojoacademy.javacore.ZZGconcorrencia.Serveice.StoreServiceDeprecated;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+public class CompletableFutureTest03 {
+    public static void main(String[] args) {
+        StoreServiceDeprecated storeServiceDeprecated = new StoreServiceDeprecated();
+        searchPriceAsyncCompletableFuture(storeServiceDeprecated);
+
+    }
+
+    private static void searchPriceAsyncCompletableFuture(StoreServiceDeprecated storeService) {
+        long start = System.currentTimeMillis();
+        ExecutorService executorService = Executors.newFixedThreadPool(6, r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            return thread;
+        });
+        
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+        List<CompletableFuture<Double>> collect = stores.stream()
+                .map(s -> CompletableFuture.supplyAsync(() -> storeService.getPriceSync(s), executorService))
+                .collect(Collectors.toList());
+
+        List<Double> doubleList = collect.stream().map(CompletableFuture::join)
+                .collect(Collectors.toList());
+        System.out.println(doubleList);
+
+        executorService.shutdown();
+        long end = System.currentTimeMillis();
+        System.out.println("Time passed to searchPricesASync " + (end - start));
+
+    }
+}
