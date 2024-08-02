f512acdbb744b0e889b8d7de11c2d815956da79a
Etanielcaus <nielfilosofia@gmail.com>
2024-08-01 21:55:51 -0300

    246 - Padrões de Projeto pt 01 - Builder

---
+++ src/academy/devdojo/devdojoacademy/javacore/ZZHpadroesdeprojetos/dominio/Person.java
@@ -0,0 +1,60 @@
+package academy.devdojo.devdojoacademy.javacore.ZZHpadroesdeprojetos.dominio;
+
+public class Person {
+    private String firstname;
+    private String lastName;
+    private String username;
+    private String email;
+
+    private Person(String firstname, String lastName, String username, String email) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.username = username;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstname='" + firstname + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static class PersonBuilder {
+        private String firstname;
+        private String lastName;
+        private String username;
+        private String email;
+
+        public PersonBuilder firstName(String firstname){
+            this.firstname = firstname;
+            return this;
+        }
+
+        public PersonBuilder lastName(String lastName){
+            this.lastName = lastName;
+            return this;
+        }
+
+        public PersonBuilder username(String username){
+            this.username = username;
+            return this;
+        }
+
+        public PersonBuilder email(String email){
+            this.email = email;
+            return this;
+        }
+
+        public Person build(){
+            return new Person(firstname, lastName, username, email);
+        }
+    }
+
+
+
+
+}
---
+++ src/academy/devdojo/devdojoacademy/javacore/ZZHpadroesdeprojetos/test/BuilderPatternTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.devdojoacademy.javacore.ZZHpadroesdeprojetos.test;
+
+import academy.devdojo.devdojoacademy.javacore.ZZHpadroesdeprojetos.dominio.Person;
+
+public class BuilderPatternTest01 {
+    public static void main(String[] args) {
+        /**
+         * The Builder pattern is a design pattern used to construct complex objects step by step. It allows
+         * you to create a representation of an object that can be incrementally built by specifying various
+         * parameters. This pattern is particularly useful
+         * when an object has a large number of optional parameters or when constructing the object is complex.
+         */
+
+        Person build = new Person.PersonBuilder()
+                .firstName("Joao")
+                .lastName("Pedro")
+                .username("joaopedro")
+                .email("joaopedro@gmail.com")
+                .build();
+
+        System.out.println(build);
+    }
+}
