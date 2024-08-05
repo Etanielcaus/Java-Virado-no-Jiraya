9d1051bc82c12f16de7fccf65a06067bdb7c2211
Etanielcaus <nielfilosofia@gmail.com>
2024-08-04 22:42:04 -0300

    256 - JDBC pt 05 - Adicionando dependência e conectando com banco

---
+++ src/main/java/academy/devdojo/devdojoacademy/javacore/ZZIjdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package academy.devdojo.devdojoacademy.javacore.ZZIjdbc.test;
+
+import academy.devdojo.devdojoacademy.javacore.ZZIjdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
