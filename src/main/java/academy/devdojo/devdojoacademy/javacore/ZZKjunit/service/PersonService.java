befa14f153f3d241d6b8cf49cfa20cb3a55673df
Etanielcaus <nielfilosofia@gmail.com>
2024-08-21 23:30:07 -0300

    281 - Testes unitários com jUnit pt 02

--- src/main/java/academy/devdojo/devdojoacademy/javacore/ZZKjunit/service/PersonService.java
+++ src/main/java/academy/devdojo/devdojoacademy/javacore/ZZKjunit/service/PersonService.java
@@ -2,9 +2,18 @@ package academy.devdojo.devdojoacademy.javacore.ZZKjunit.service;
 
 import academy.devdojo.devdojoacademy.javacore.ZZKjunit.dominio.Person;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class PersonService {
 
     public static boolean isAdult(Person person){
+        Objects.requireNonNull(person, "person can't be null");
         return person.getAge() >= 18;
+    }
+
+    public static List<Person> personList(List<Person> personListening){
+        return personListening.stream().filter(PersonService::isAdult).collect(Collectors.toList());
     }
 }
--- src/test/java/academy/devdojo/devdojoacademy/javacore/ZZKjunit/service/PersonServiceTest.java
+++ src/test/java/academy/devdojo/devdojoacademy/javacore/ZZKjunit/service/PersonServiceTest.java
@@ -2,15 +2,49 @@ package academy.devdojo.devdojoacademy.javacore.ZZKjunit.service;
 
 import academy.devdojo.devdojoacademy.javacore.ZZKjunit.dominio.Person;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class PersonServiceTest {
+import java.util.List;
 
-    @DisplayName("Test to show if the Person is true")
+class PersonServiceTest {
+    private Person isAdult;
+    private Person isNotAdult;
+    private PersonService personService = new PersonService();
+
+    @BeforeEach
+    public void setUp(){
+        isAdult = new Person(18);
+        isNotAdult = new Person(15);
+    }
+
+
+    @DisplayName("Should Person is lower then 18 ")
     @Test
-    void isAdult() {
-        Person person = new Person(15);
-        Assertions.assertFalse(PersonService.isAdult(person));
+    void person_notIsAdult() {
+        Assertions.assertFalse(personService.isAdult(isNotAdult));
+    }
+
+    @DisplayName("Should Person is greater then 18")
+    @Test
+    void person_isAdult() {
+        Assertions.assertTrue(personService.isAdult(isAdult));
+    }
+
+    @DisplayName("Should Throw NullPointerException")
+    @Test
+    void nullPointerExceptioni() {
+        Assertions.assertThrows(NullPointerException.class ,() -> personService.isAdult(null));
+    }
+
+    @DisplayName("Should have two Persons is Adult")
+    @Test
+    void listPerson() {
+        Person person = new Person(16);
+        Person person1 = new Person(18);
+        Person person2 = new Person(19);
+        List<Person> person11 = List.of(person1, person2, person);
+        Assertions.assertEquals(2, personService.personList(person11).size());
     }
 }
